8ff5d2a78555217a5eacc90c8314b3ec9cc66c12
VSBernhard <125886388+VSBernhard@users.noreply.github.com>
2024-03-12 13:15:16 +0100

    Add files via upload

---
+++ Bitlocker_disable.ps1
@@ -0,0 +1,15 @@
+﻿# Remediation Script to Disable BitLocker Encryption on All Volumes
+
+$encryptionStatus = Get-BitLockerVolume
+
+if ($encryptionStatus -eq $null) {
+    # BitLocker is not enabled on any volume
+    Write-Host "BitLocker is not enabled on any volume. No action needed."
+    exit 0
+} else {
+    # BitLocker is enabled on at least one volume
+    Write-Host "Disabling BitLocker on all volumes..."
+    Disable-BitLocker -MountPoint $encryptionStatus.MountPoint -Confirm:$false
+    Write-Host "BitLocker has been disabled on all volumes."
+    
+}
