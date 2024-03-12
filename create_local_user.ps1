04cdeb1d078f6e9ae43b5ff0831b476e9596fa66
VSBernhard <125886388+VSBernhard@users.noreply.github.com>
2024-03-12 11:09:31 +0100

    Add files via upload

---
+++ create_local_user.ps1
@@ -0,0 +1,9 @@
+﻿$LocalUser = "Admin"
+$Password = ConvertTo-SecureString "!admin!" -AsPlainText -Force
+Function Create_LocalWindowsAccount
+{
+    New-LocalUser $LocalUser -Password $Password -FullName "Lokaler Admin" -Description "Localer Administrator Account"
+    Add-LocalGroupMember -Group "Administratoren" -Member $LocalUser
+    Set-LocalUser -Name $LocalUser -PasswordNeverExpires:$true
+}
+Create_LocalWindowsAccount
