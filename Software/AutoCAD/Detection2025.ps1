47a2cc45ea8b519ec7603b8f6eae7f6e6d923d6d
VSBernhard <125886388+VSBernhard@users.noreply.github.com>
2024-05-01 08:59:49 +0200

    Update Detection2025.ps1

--- Software/AutoCAD/Detection2025.ps1
+++ Software/AutoCAD/Detection2025.ps1
@@ -1,1 +1,8 @@
+If([Version](Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{28B89EEF-8101-0407-2102-CF3F3A09B77D}' -Name DisplayVersion -ea SilentlyContinue) -ge '25.0.58.0') {
+Write-Host "Installed"
+Exit 0
+}
+else {
+Exit 1
+}
 
